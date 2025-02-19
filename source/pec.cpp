29c82270f0972d2aecb9301bb201246bee7398ef
Erencan Ceyhan <erencan.ceyhan@hotmail.com>
2025-02-19 12:34:36 +0300

    Migrate to CLion

--- source/pec.cpp
+++ source/pec.cpp
@@ -6,18 +6,18 @@ int main() {
     auto scanner = yyscan_t();
     yylex_init(&scanner);
     auto loc = pec::parser::location_type();
-    auto filename = std::string("stream");
+    const auto filename = std::string("stream");
     loc.begin.initialize(&filename, 1, 1);
     loc.end.initialize(&filename, 1, 1);
     yyset_extra(loc, scanner);
-    auto data = std::string(
+    const auto data = std::string(
         "let x = (Int32 a) + !(b + c);\n"
         "var y = int64 Float x / g;"
     );
-    auto buf = yy_scan_bytes(data.data(), data.size(), scanner);
+    const auto buf = yy_scan_bytes(data.data(), data.size(), scanner);
     // yy_switch_to_buffer(buf, scanner);
     auto parser = pec::parser(scanner);
-    int x = parser.parse();
+    const int x = parser.parse();
     std::cout << "Result: " << x << "\n";
     yy_delete_buffer(buf, scanner);
     yylex_destroy(scanner);
